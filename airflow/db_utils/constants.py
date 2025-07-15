985b2afa111a10a70bb8c15d793f9ca4fd763ac0
chunyu <1244315855@qq.com>
2025-07-15 20:22:22 +0000

    feat: SEC Filings Storage System Design for with MongoDB completed.

--- airflow/db_utils/constants.py
+++ airflow/db_utils/constants.py
@@ -1,13 +1,17 @@
 # MongoDB connection details
 MONGO_USERNAME = "root"  # Replace with your MongoDB username
 MONGO_PASSWORD = "root-1234"  # Replace with your MongoDB password
-MONGO_HOST = "mongo_container"  # Replace with the hostname or IP address
+
+MONGO_HOST = "mongo_container" # run in the docker
+# MONGO_HOST = "localhost"  # Uncomment this line if running locally
+
 MONGO_PORT = 27017  # Default MongoDB port
 DB_NAME = "financial_db"  # Database name
 COMPANY_PERMID_DB = "company_permid"
 COMPANY_TICKER_DATA = "company_ticker"
 ANNUAL_REPORT_COLLECTION = "annual_reports"
 QUARTER_REPORT_COLLECTION = "quarter_reports"
+SEC_REPORT_COLLECTION = "sec_reports"
 TRANSCRIPTS_COLLECTION = "transcripts"
 NEWS_COLLECTION = "news_articles"
-PRICE_COLLECTION = "price_data"
+PRICE_COLLECTION = "price_data" 
