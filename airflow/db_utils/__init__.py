dc48963ad53fc4f2ed46f0faa2c8ec0f6351f24c
007seann <007seann@gmail.com>
2025-07-20 08:19:57 +0000

    from feature/mongo_storage

--- airflow/db_utils/__init__.py
+++ airflow/db_utils/__init__.py
@@ -2,3 +2,4 @@ from .push_sec_reports import push_sec_reports
 from .constants import *
 from .mongo_utils import get_db, convert_objectid, save_json
 from .search_db import query_sec_reports
+from .push_transcripts import merge_transcripts
